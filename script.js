8267d095ceda883974a909e5db34dbf66bfba13f
arora <aroramadhav65@gmail.com>
2022-06-14 20:45:15 +0530

    Update

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ let sc = document.querySelector('#hc');
 let day = new Date();
 let hh = day.getHours() * 30;
 let mm = day.getMinutes() * 30;
-let ss = day.getMinutes() * 30;
+let ss = day.getSeconds() * 30;
